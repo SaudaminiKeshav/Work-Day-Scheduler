f79c609ccc0c09e0ed8620280036c54aa899a850
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-07-13 15:48:49 -0700

    Added code logic to save the title and desc enetered in the dialog to a an array of object on clicking save and hidding dialog on clicking save or delete

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ var timeArray = [
 ]
 
 var taskArray = [];
-taskArray.push({ timeKey: "4:00 AM", title: "Task 1", Category: "No Category", Priority: "Low" });
-taskArray.push({ timeKey: "5:00 AM", title: "Task 2", Category: "No Category", Priority: "High" });
-taskArray.push({ timeKey: "6:00 AM", title: "Task 3", Category: "No Category", Priority: "Low" });
+// taskArray.push({ timeKey: "4:00 AM", title: "Task 1", Category: "No Category", Priority: "Low" });
+// taskArray.push({ timeKey: "5:00 AM", title: "Task 2", Category: "No Category", Priority: "High" });
+// taskArray.push({ timeKey: "6:00 AM", title: "Task 3", Category: "No Category", Priority: "Low" });
 
 var timeKey = [];
 // Creating a div for details dialog 
@@ -36,87 +36,10 @@ taskDetailsDialog.attr("class", "task-details-dialog");
 $(document).ready(function () {
     getAndDisplayDate();
     createAndDisplayTaskDetailsDialog();
-
 });
 
-function createAndDisplayTaskDetailsDialog() {
-
-    // Creating a div to contain the title and hr of the details dialog 
-    var divSection1 = $("<div>");
-    divSection1.attr("id", "div-section-1");
-
-    // Title of the dialog 
-    var detailsTitle = $("<input>");
-    detailsTitle.attr("class", "details-title");
-    detailsTitle.attr("type", "text");
-    detailsTitle.attr("placeholder", "Enter Title");
-    detailsTitle.on('input', function () {
-        console.log(detailsTitle.val());
-    });
-
-
-    // Hr separation after the title 
-    var detailsHr1 = $("<hr>");
-    detailsHr1.attr("class", "details-hr");
-
-    // Adding the title and hr to div 
-    divSection1.append(detailsTitle);
-    divSection1.append(detailsHr1);
-
-    // Creating a div to contain the task description text area
-    var divSection2 = $("<div>");
-    divSection2.attr("id", "div-section-2");
-
-    // Title of the dialog 
-    var detailsDesc = $("<TEXTAREA>");
-    detailsDesc.attr("class", "details-desc");
-    detailsDesc.attr("type", "text");
-    detailsDesc.attr("rows", "15");
-    detailsDesc.attr("cols", "95");
-    detailsDesc.attr("placeholder", "Enter task description");
-    detailsDesc.on('input', function () {
-        console.log(detailsDesc.val());
-    });
-
-    // Adding the title and hr to div 
-    divSection2.append(detailsDesc);
-
-    // Creating a div to contain the save and delete buttons
-    var divSection3 = $("<div>");
-    divSection3.attr("id", "div-section-3");
-
-    // Hr separation after the title 
-    var detailsHr2 = $("<hr>");
-    detailsHr2.attr("class", "details-hr");
-
-    var saveButton = $("<button>");
-    saveButton.attr("class", "saveBtn");
-    saveButton.text("Save");
-    saveButton.on('click', function saveFunction() {
-        console.log(detailsTitle.val());
-        console.log(detailsDesc.val());
-    });
-
-    var deleteButton = $("<button>");
-    deleteButton.attr("class", "saveBtn");
-    deleteButton.text("Delete");
-    deleteButton.on('click', function deleteFunction() {
-        $(".task-details-dialog").hide();
-    });
-
-    divSection3.append(detailsHr2);
-    divSection3.append(saveButton);
-    divSection3.append(deleteButton);
-
-    // Add the first div dection to the details dialog div 
-    taskDetailsDialog.append(divSection1);
-    taskDetailsDialog.append(divSection2);
-    taskDetailsDialog.append(divSection3);
-}
-
-
 function getAndDisplayDate() {
-    // get and display date 
+    // get and display title date 
     $(".time-block").append(getH2DateElement(createAndDisplayDate()));
 
     createHourlyPlanDiv();
@@ -135,9 +58,97 @@ function createAndDisplayDate() {
 
 function createHourlyPlanDiv() {
     timeArray.forEach(time => {
-        $(".time-schedule-div").append(getTimeTaskDiv(getTimeBlockElement(time), getTaskBlockElement()));
+        $(".time-schedule-div").append(getTimeTaskDiv(getTimeBlockElement(time), getTaskBlockElement(time)));
     });
 
+}
+
+
+
+function createAndDisplayTaskDetailsDialog() {
+
+    // Creating a div to contain the title and hr of the details dialog 
+    var divSection1 = $("<div>");
+    divSection1.attr("id", "div-section-1");
+
+    // Title of the dialog 
+    var detailsTitle = $("<input>");
+    detailsTitle.attr("class", "details-title");
+    detailsTitle.attr("type", "text");
+    detailsTitle.attr("placeholder", "Enter Title");
+    detailsTitle.on('input', function () {
+        // console.log(detailsTitle.val());
+    });
+
+
+    // Hr separation after the title 
+    var detailsHr1 = $("<hr>");
+    detailsHr1.attr("class", "details-hr");
+
+    // Display the time for which the task is created 
+    var detailsTimeKey = $("<p>");
+    detailsTimeKey.attr("id", "details-timekey");
+    detailsTimeKey.text("timekey");
+
+    // Adding the title and hr to div 
+    divSection1.append(detailsTitle);
+    divSection1.append(detailsTimeKey);
+    divSection1.append(detailsHr1);
+
+    // Creating a div to contain the task description text area
+    var divSection2 = $("<div>");
+    divSection2.attr("id", "div-section-2");
+
+    // Title of the dialog 
+    var detailsDesc = $("<TEXTAREA>");
+    detailsDesc.attr("class", "details-desc");
+    detailsDesc.attr("type", "text");
+    detailsDesc.attr("rows", "15");
+    detailsDesc.attr("cols", "95");
+    detailsDesc.attr("placeholder", "Enter task description");
+    detailsDesc.on('input', function () {
+        // console.log(detailsDesc.val());
+    });
+
+    // Adding the title and hr to div 
+    divSection2.append(detailsDesc);
+
+    // Creating a div to contain the save and delete buttons
+    var divSection3 = $("<div>");
+    divSection3.attr("id", "div-section-3");
+
+    // Hr separation after the title 
+    var detailsHr2 = $("<hr>");
+    detailsHr2.attr("class", "details-hr");
+
+    var saveButton = $("<button>");
+    saveButton.attr("class", "saveBtn");
+    saveButton.text("Save");
+    saveButton.on('click', function saveFunction() {
+        taskArray.push({ timeKey: `${detailsTimeKey.text()}`, title: `${detailsTitle.val()}`, desc: `${detailsDesc.val()}`, Category: "No Category", Priority: "Low" })
+
+        console.log(taskArray);
+
+        detailsTitle.val("");
+        detailsDesc.val("");
+        $(".task-details-dialog").hide();
+    });
+
+    var deleteButton = $("<button>");
+    deleteButton.attr("class", "saveBtn");
+    deleteButton.text("Delete");
+    deleteButton.on('click', function deleteFunction() {
+        $(".task-details-dialog").hide();
+    });
+
+    divSection3.append(detailsHr2);
+    divSection3.append(saveButton);
+    divSection3.append(deleteButton);
+
+    // Add the first div dection to the details dialog div 
+    taskDetailsDialog.append(divSection1);
+    taskDetailsDialog.append(divSection2);
+    taskDetailsDialog.append(divSection3);
 }
 
 // getTimeKey(time) 
@@ -163,22 +174,23 @@ function getTimeTaskDiv(time, task) {
 function getTimeBlockElement(time) {
     var newh6 = $("<h6>");
     newh6.attr("class", "time-schedule");
+    newh6.attr("id", `${time}`);
     newh6.text(time);
     return newh6;
 }
 
-function getTaskBlockElement() {
+function getTaskBlockElement(time) {
 
-    var titleElement = getPElement()
+    var titleElement = getPElement();
 
     // Creating new div to display title and tags 
     var newDiv = $("<div>");
     newDiv.attr("class", "task-schedule");
-    newDiv.attr("id", "task-element")
+    newDiv.attr("id", `${time}`);
     newDiv.append(titleElement);
 
     // Adding click listener to the task div 
-    addTaskClickListener();
+    addTaskClickListener(time);
     return newDiv;
 }
 
@@ -188,10 +200,12 @@ function getPElement() {
     return newP;
 }
 
-function addTaskClickListener() {
+function addTaskClickListener(time) {
     $(".task-schedule").on('click', function () {
-        $("body").append(taskDetailsDialog)
-        $(".task-details-dialog").show();
+        console.log($(this).attr('id'));
+        $("body").append(taskDetailsDialog);
+        $(".task-details-dialog").show()
+        $("#details-timekey").text($(this).attr('id'));
     })
 }
 
